7bc8a44d239dbfdb51023813044a200038b769c5
Priyanshu <priyanshu21416@gmail.com>
2026-01-17 19:15:16 +0530

    Deploying Frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,15 +6,19 @@ import resumeRoutes from "./routes/resumeRoutes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: [
       "http://localhost:5173",
-      "https://cruxcv.onrender.com/"
+      "https://cruxcv.onrender.com"
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  }
-));
+  })
+);
+app.options("*", cors());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
