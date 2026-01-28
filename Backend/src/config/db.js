9fa496592b6b8741a0a7f2d6f792597def327cfe
Priyanshu <priyanshu21416@gmail.com>
2026-01-28 16:50:04 +0530

    Frontend changes

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
+    console.log("👉 DB NAME USED:", mongoose.connection.name);
   } catch (error) {
     console.error("MongoDB connection failed", error.message);
     process.exit(1);
